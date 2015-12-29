a76168f54a708919fdbb3661081a86b3d5d2ed37
Barry Jones <barry_jones@androtech.com>
2015-12-29 11:05:23 +0000

    updated sync data; updated json.net 6.0.4

--- CloudSyncModel/Menus/StoreMenuUpdates.cs
+++ CloudSyncModel/Menus/StoreMenuUpdates.cs
@@ -6,9 +6,20 @@ namespace CloudSyncModel.Menus
     {
         public StoreMenuUpdates() 
         {
+            this.MenuChanges = new List<StoreMenuUpdate>();
             this.MenuThumbnailChanges = new List<StoreMenuUpdate>();
-        } 
+        }
 
+        /// <summary>
+        /// Gets or sets the menu changes.
+        /// </summary>
+        /// <value>The menu changes.</value>
+        public List<StoreMenuUpdate> MenuChanges { get; set; }
+
+        /// <summary>
+        /// Gets or sets the menu thumbnail changes.
+        /// </summary>
+        /// <value>The menu thumbnail changes.</value>
         public List<StoreMenuUpdate> MenuThumbnailChanges { get; set; }
     }
 }
