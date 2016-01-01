df72a101f54732a354eb8a5f80d49ed17b658a74
Barry Jones <barry_jones@androtech.com>
2016-01-01 19:38:00 +0000

    sync stage done, and dataversions implemented.

--- CloudSync/Extensions/HostV2Models.cs
+++ CloudSync/Extensions/HostV2Models.cs
@@ -1,4 +1,5 @@
 ﻿using CloudSyncModel.HostV2;
+using CloudSyncModel.StoreDeviceModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,39 @@ namespace CloudSync.Extensions
             {
                 ApplicationId = model.ApplicationId,
                 HostId = model.HostId
+            };
+        }
+    }
+
+    public static class StoreDevicesExtensions 
+    {
+        public static ExternalApiScaffold ToSyncModel(this AndroAdminDataAccess.EntityFramework.ExternalApi model) 
+        {
+            return new ExternalApiScaffold() 
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Parameters = model.Parameters
+            };
+        }
+
+        public static DeviceScaffold ToSyncModel(this AndroAdminDataAccess.EntityFramework.Device model) 
+        {
+            return new DeviceScaffold()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                ExternalApiId = model.ExternalApiId
+            };
+        }
+
+        public static SiteDeviceScaffold ToSyncModel(this AndroAdminDataAccess.EntityFramework.StoreDevice model) 
+        {
+            return new SiteDeviceScaffold()
+            {
+                AndromedaSiteId = model.Store.AndromedaSiteId,
+                DeviceId = model.DeviceId,
+                Parameters = model.Parameters
             };
         }
     }
